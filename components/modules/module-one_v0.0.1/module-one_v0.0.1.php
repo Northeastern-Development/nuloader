74e8b2fe8dcce253a1a219c1c1c8b2bffbff39d0
Marcy, Kyle <k.marcy@northeastern.edu>
2018-10-04 15:04:46 -0400

    cleaning up and honing in the functionality; commenting etc

--- components/modules/module-one_v0.0.1/module-one_v0.0.1.php
+++ components/modules/module-one_v0.0.1/module-one_v0.0.1.php
@@ -1,17 +1,60 @@
 <?php
-/**
- *  Ensure Search Page Exists
- */
+    /**
+     * Ensure site has a 'search' page that is hidden from users
+     */
     class SearchModule
     {
-        // set required vars
-        var $someVars;
-
         function __construct(){
-            
             add_action('admin_init', array($this, 'check_for_search_page'));
-            
+            add_action('pre_get_posts', array($this, 'lock_search_page'));
+            $this->override_page_template();
+        }        
+        /**
+         * check for the search page; creating it if required
+         */
+        function check_for_search_page(){
+            $searchPage = get_page_by_title('Search', ARRAY_A);
+            if( !empty( $searchPage ) ){
+                if( $searchPage['post_status'] === 'trash' ){
+                    error_log(print_r( "Search Page was found with status: \n" . $searchPage['post_status'], true));
+                    // post in trash
+                }
+                else if( $searchPage['post_status'] === 'publish' ){
+                    error_log(print_r( "Search Page was found with status: \n" . $searchPage['post_status'], true));
+                    // post is published
+                    // check its page template,
+                }
+                else if( $searchPage['post_status'] != 'publish' ){
+                    // post is either pending, draft, auto-draft, future, private, or 'inherit'
+                    error_log(print_r( "Search Page was found with status: \n" . $searchPage['post_status'], true));
+                }
+            }
+            // page will not be created if in the trash
+            else if( empty($searchPage) ){
+                $this->do_insert_search_page();
+            }
 
+        }
+        /**
+         * create the search page
+         */
+        function do_insert_search_page(){
+            // there is no search page
+            $user_id = get_current_user_id();
+            // default args for wp_insert_post
+            $args = array(
+                'post_author' => $user_id,
+                'post_title' => 'Search',
+                'post_type' => 'page',
+                'post_status' => 'publish',
+            );
+            // return ID on success or 0 on fail
+            $success = wp_insert_post($args);
+        }
+        /**
+         * override the page template for the search page
+         */
+        function override_page_template(){
             add_filter( 'page_template', 'override_default_page_template' );
             function override_default_page_template( $page_template )
             {
@@ -21,83 +64,26 @@
                 return $page_template;
             }
         }
-
-        function check_for_search_page(){
-            
-            $searchPage = get_page_by_title('Search', ARRAY_A);
-
-            if( !empty( $searchPage ) ){
-
-                if( $searchPage['post_status'] === 'trash' ){
-                    error_log(print_r( "Search Page was found with status: \n" . $searchPage['post_status'], true));
-                    // post in trash
-                }
-                
-                else if( $searchPage['post_status'] === 'publish' ){
-                    error_log(print_r( "Search Page was found with status: \n" . $searchPage['post_status'], true));
-                    // post is published
-                    // check its page template,
-                    
-                }
-
-                else if( $searchPage['post_status'] != 'publish' ){
-                    // post is either pending, draft, auto-draft, future, private, or 'inherit'
-                    error_log(print_r( "Search Page was found with status: \n" . $searchPage['post_status'], true));
-                }
+        /**
+         * hides the search page in the admin area
+         * users can be whitelisted by ID if required
+         *
+         * @param object $wp_query
+         */
+        function lock_search_page($query){
+            if( !is_admin() && !is_main_query() ){
+                return;
             }
-
-            // page will not be created if in the trash
-            else if( empty($searchPage) ){
-                $this->do_insert_search_page();
+            // allow certain users to see the hidden page
+            $master_user_id = 1;
+            if( $master_user_id === get_current_user_id() ){
+                return;
             }
-
-        }
-
-        function do_insert_search_page(){
-         
-            // there is no search page
-            $user_id = get_current_user_id();
-         
-            // default args for wp_insert_post
-            $args = array(
-                'post_author' => $user_id,
-                'post_title' => 'Search',
-                'post_type' => 'page',
-                'post_name' => '',      // will default to serialized title
-                'post_status' => 'publish',
-                'post_content' => '',
-                'post_content_filtered' => '',
-                'post_excerpt' => '',
-                'comment_status' => '',
-                'ping_status' => '',
-                'post_password' => '',
-                'to_ping' =>  '',
-                'pinged' => '',
-                'post_parent' => 0,
-                'menu_order' => 0,
-                'guid' => '',
-                'import_id' => 0,
-                'context' => '',
-            );
-         
-            // return ID on success or 0 on fail
-            $success = wp_insert_post($args);
-
-            if( $success ){
-                $this->do_configure_search_page($success);
+            global $typenow;
+            if( 'page' === $typenow ){
+                $query->set('post__not_in', array(get_page_by_title('Search', ARRAY_A)['ID']));
             }
         }
-
-
-        function do_configure_search_page($page_id){
-
-        }
-
-
-
-
     }
-
     $searchPageBuilder = new SearchModule();
-
 ?>
--- components/modules/module-one_v0.0.1/search-page-template.php
+++ components/modules/module-one_v0.0.1/search-page-template.php
@@ -1,11 +1,19 @@
-<?php /* Template Name: Search Template */ ?>
+<?php
+/*
+Template Name: Search Template
+*/
+if(!isset($_GET['query']) || $_GET['query'] == ''){
+    header('location:/search/?query=northeastern university');
+    exit();
+}
+    get_header();
+ ?>
+    <main>
 
+        <script src="https://search.northeastern.edu/nuglobalutils/requests/js/globalsearch.js"></script>
+
+
+    </main>
 <?php 
-
-
-echo "<h1>TEST</h1>";
-
-
-
-
-?>
+    get_footer();
+ ?>
